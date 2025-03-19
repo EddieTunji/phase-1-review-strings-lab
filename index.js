6cd045ade68e12a1e128f9ce161a161a04b2c504
EddieTunji <davidlundi24@gmail.com>
2025-03-19 15:11:57 +0300

    strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+/* let excitedWelcomeMessage = `WELCOME TO FLATBOOK,`; */
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
